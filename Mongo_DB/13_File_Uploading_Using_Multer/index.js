ae6a841baf5e43f0af972a18ac7a7c5a4c4b6598
Shiraz Mazhar <imshiraz007@gmail.com>
2024-06-27 00:01:59 +0500

    File_Uploading_Multer

---
+++ Mongo_DB/13_File_Uploading_Using_Multer/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+import multer from "multer";
+import mongoose from "mongoose";
+import fileRouter from "./routes/file.js";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/uploads", express.static("uploads")); 
+app.use("/file",fileRouter)
+
+const connection = mongoose.connection;
+connection.once("connected", () => console.log("Database Connected ~"));
+connection.on("error", (error) => console.log("Database Error: ", error));
+
+
+mongoose.connect("mongodb://localhost:27017/db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.listen(1500);
---
+++ Mongo_DB/13_File_Uploading_Using_Multer/routes/file.js
@@ -0,0 +1,90 @@
+import express from "express";
+const router = express.Router();
+import upload from "../middlewares/upload.js";
+import FileModel from "../models/file.js";
+
+router.get("/", async (req, res) => {
+    try {
+        const data = await FileModel.find();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const data = await FileModel.findById(req.params.id);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.post("/", upload.single("file"), async (req, res) => {
+    try {
+        if (req.file) {
+            req.file.path = req.file.path.replace(`\\`, `/`); 
+            const file = {
+                link: `${req.protocol}://${req.get("host")}/${req.file.path}`,
+                name: req.file.filename,
+                original_name: req.file.originalname,
+                type: req.file.mimetype,
+                path: req.file.path,
+            };
+            const data = await FileModel.create(file);
+            res.status(200).json(data);
+        } else {
+            res.status(400).send("no file found");
+        }
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+router.patch("/:id", async (req, res) => {
+    try {
+        if (req.file) {
+            req.file.path = req.file.path.replace(`\\`, `/`);
+
+            const file = {
+                link: `${req.protocol}://${req.get("host")}/${req.file.path}`,
+                name: req.file.filename,
+                original_name: req.file.originalname,
+                type: req.file.mimetype,
+                path: req.file.path,
+            };
+            const data = await FileModel.findByIdAndUpdate(req.params.id, file);
+            res.status(200).json(data);
+        } else {
+            res.status(400).send("no file found");
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        console.log(req.params.id);
+        const data = await FileModel.findOneAndDelete({
+            _id:req.params.id
+        });
+        if (data) {
+            res.status(200).json({
+                message: "File deleted successfully"
+            });
+        } else {
+            res.status(404).json({
+                message: "File not found"
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+});
+
+export default router;
